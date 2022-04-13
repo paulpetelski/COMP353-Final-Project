6faec04b4f4a979d9a5fc40a231cb774d871cbb8
paulpetelski <petelskipaul@gmail.com>
2022-04-13 12:23:31 -0500

    card formatting

--- flaskDemo/models.py
+++ flaskDemo/models.py
@@ -6,5 +6,22 @@ from sqlalchemy import orm
 
 db.Model.metadata.reflect(db.engine)
 
+# add tables
 class Product(db.Model):
     __table__ = db.Model.metadata.tables['product']
+class Book(db.Model):
+    __table__ = db.Model.metadata.tables['book']
+class Customer(db.Model):
+    __table__ = db.Model.metadata.tables['customer']
+class Department(db.Model):
+    __table__ = db.Model.metadata.tables['department']
+class Employee(db.Model):
+    __table__ = db.Model.metadata.tables['employee']
+class Orderline(db.Model):
+    __table__ = db.Model.metadata.tables['orderline']
+class Orders(db.Model):
+    __table__ = db.Model.metadata.tables['orders']
+class Publisher(db.Model):
+    __table__ = db.Model.metadata.tables['publisher']
+class Subscription(db.Model):
+    __table__ = db.Model.metadata.tables['subscription']
