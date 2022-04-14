da402e299322ca85d97afea6e7f93863f537abc0
paulpetelski <petelskipaul@gmail.com>
2022-04-14 18:58:40 -0500

    deleted post

--- flaskDemo/models.py
+++ flaskDemo/models.py
@@ -39,19 +39,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
-
-class Post(db.Model):
-     __table_args__ = {'extend_existing': True}
-     id = db.Column(db.Integer, primary_key=True)
-     title = db.Column(db.String(100), nullable=False)
-     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-     content = db.Column(db.Text, nullable=False)
-     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-     def __repr__(self):
-         return f"Post('{self.title}', '{self.date_posted}')"
